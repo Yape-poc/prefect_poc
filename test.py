beb69a6698abe1457f1bfc1f671b15179a1f273f
Bryan Montoya <55662285+bmontoyag@users.noreply.github.com>
2025-11-03 11:37:43 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,21 @@
-pip install "prefect[databricks]"
+import sys
+import subprocess
 
-from prefect_databricks import DatabricksCredentials
-databricks_credentials_block = DatabricksCredentials.load("test")
+# Install prefect with databricks if not already installed
+subprocess.check_call([sys.executable, "-m", "pip", "install", "prefect[databricks]"])
 
+from prefect import flow
+from prefect_databricks import DatabricksCredentials, jobs_list
 
 @flow
 def example_execute_endpoint_flow():
     databricks_credentials = DatabricksCredentials.load("test")
     jobs = jobs_list(
-        databricks_credentials,
+        databricks_credentials=databricks_credentials,
         limit=5
     )
     return jobs
 
 if __name__ == "__main__":
-    example_execute_endpoint_flow()
+    result = example_execute_endpoint_flow()
+    print(result)
